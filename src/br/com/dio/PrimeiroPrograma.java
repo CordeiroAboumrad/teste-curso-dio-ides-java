042ec60798a988bbed4034a9a6964fb99135d201
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-02-15 10:13:51 -0300

    Addition of calculator program.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,15 +7,10 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		Gato gato = new Gato();
 		Livro livro = new Livro();
-		
-		
+				
 		System.out.println(gato);
 		System.out.println(livro);
 		
-		
-		/*int a = 2;
-		int b = 3;
-		System.out.println("Hello, world! " + (a + b));*/
 	}
 
 }
